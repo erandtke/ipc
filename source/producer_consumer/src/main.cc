79ecef2e88bf7cc3adc94e168d7c0cc4a9d10c36
Ed Randtke <erandtke@gmail.com>
2024-02-18 13:31:29 -0500

    add producer and consumer

--- source/producer_consumer/src/main.cc
+++ source/producer_consumer/src/main.cc
@@ -1,3 +1,55 @@
+#include <iostream>
+#include <atomic>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <thread>
+
+struct Item
+{
+    int i;
+};
+
+/*
+std::ostream &operator << (std::ostream &os, const Item &item)
+{
+    os << "i: " << item.i;
+    return os;
+}
+*/
+
+void Producer(int numItems, std::queue<Item> &sharedQueue, std::mutex &m, std::atomic<bool> &completed)
+{
+    int counter = 0;
+    while(--numItems)
+    {
+        auto item = Item{.i = counter++};
+        std::cout << "producer: " << item.i << std::endl;
+        {
+            std::lock_guard<std::mutex> lock(m);
+            sharedQueue.push(item);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds{100});
+    }
+
+    completed = true;
+}
+
+void Consumer(std::queue<Item> &sharedQueue, std::mutex &m, std::atomic<bool> completed)
+{
+    while(completed == false)
+    {
+        std::optional<Item> item;
+        {
+            std::lock_guard<std::mutex> lock(m);
+            item = sharedQueue.front();
+            sharedQueue.pop();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds{50});
+        std::cout << "consumer: " << item->i << std::endl;
+    }
+}
+
 int main(int argc, char** argv)
 {
     return 0;
